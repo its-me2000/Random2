29acde62c09b54e7af73568bfca586fb83795b24
its-me2000 <Eugenijus@Eugenijuss-iMac.local>
2017-11-28 11:02:16 +0100

    Initial Commit

---
+++ Random2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Random2
+//
+//  Created by Eugenijus Margalikas on 28/11/2017.
+//  Copyright © 2017 Eugenijus Margalikas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
